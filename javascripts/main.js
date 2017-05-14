c7327ac6424735165f132fd6a71d7b9e4e2ee249
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-13 22:01:11 -0500

    search and filter works fine

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,46 @@
-app.run((FIREBASE_CONFIG)=>{
-	firebase.initializeApp(FIREBASE_CONFIG);
+app.run((FIREBASE_CONFIG) => {
+    firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-// app.controller("itemCtrl")
+app.controller("itemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+    $scope.showMushroomsList = false;
+    $scope.mushrooms = [];
+
+    let getItemList = () => {
+        let mushroomz = [];
+        return new $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+                .then((fbItems) => {
+                    var itemCollection = fbItems.data;
+                    Object.keys(itemCollection).forEach((key) => {
+                        itemCollection[key].id = key;
+                        mushroomz.push(itemCollection[key]);
+                    });
+                    resolve(mushroomz);
+                    console.log("mushroomz in getItemList function", mushroomz);
+                })
+                .catch((error) => {
+                    reject(error);
+                    console.log("error in getItemList function :", error);
+                });
+        });
+    };
+
+    let getItems = () => {
+        getItemList()
+            .then((mushroomz) => {
+                $scope.mushrooms = mushroomz;
+            }).catch((error) => {
+                console.log("error in getItems function :", error);
+            });
+    };
+
+    getItems();
+
+    $scope.showSearch = () => {
+        $scope.showMushroomsList = true;
+        console.log(" you click showSearch");
+    };
+
+
+});
